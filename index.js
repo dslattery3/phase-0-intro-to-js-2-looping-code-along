07573b0cd025964d35c932eef6659e4806919511
Dalton <slattery.da@gmail.com>
2021-12-08 16:54:42 -0600

    Completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const giftsFrom = ['sarah','mom'];
+function writeCards(names, gift) {
+    let length = names.length;
+    let thankYouCards = [];
+    for (let x = 0; x < length; x++) {
+        thankYouCards[x] = `Thank you, ${names[x]}, for the wonderful ${gift} gift!`;
+    }
+    return thankYouCards;
+}
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n);
+        n--;
+    }
+}
